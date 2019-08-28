d3cd26df260e2c90831bf973cf6fc336fd232075
Wouter Geraedts <git@woutergeraedts.nl>
2019-08-28 22:37:11 +0200

    Added SessionResult fields

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,28 @@ mod session;
 pub use crate::request::*;
 pub use crate::session::*;
 
-use futures::future::Future;
+use futures::future::{loop_fn, ok, Either, Future, Loop};
+use std::time::{Duration, Instant};
+use tokio::timer::Delay;
+
+fn print_qr(sp: &SessionPackage) {
+    let s = serde_json::to_string(&sp.session_ptr).unwrap();
+
+    let code = qrcode::QrCode::new(s).unwrap();
+    let scode = code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+
+    println!("\n\n{}", scode);
+}
 
 fn main() {
     let dr = DisclosureRequest {
         disclose: AttributeConDisCon(vec![AttributeDisCon(vec![AttributeCon(vec![
             AttributeRequest {
-                atype: "pbdf.pbdf.diploma.degree".to_string(),
+                atype: "pbdf.pbdf.email.email".to_string(),
                 value: None,
                 not_null: true,
             },
@@ -23,26 +38,27 @@ fn main() {
 
     let res = client
         .request(&dr)
-        .and_then(move |r| client.result(&r.token));
-    /*.map(|resp| {
-        println!("status: {:?}", resp);
+        .and_then(move |sp| {
+            print_qr(&sp);
 
-        let resp2 = serde_json::to_string(&resp.session_ptr).unwrap();
-
-        let code = qrcode::QrCode::new(resp2).unwrap();
-        let scode = code
-            .render::<char>()
-            .quiet_zone(false)
-            .module_dimensions(2, 1)
-            .build();
-
-        println!("\n\n{}", scode);
-    });*/
+            loop_fn(120, move |i: u8| {
+                client.result(&sp.token).and_then(move |r: SessionResult| {
+                    if r.status != SessionStatus::Done && i > 0 {
+                        Either::A(
+                            Delay::new(Instant::now() + Duration::new(0, 500_000_000))
+                                .then(move |_| Ok(Loop::Continue(i - 1))),
+                        )
+                    } else {
+                        Either::B(ok(Loop::Break(r)))
+                    }
+                })
+            })
+        })
+        .and_then(|r: SessionResult| {
+            println!("{:?}", r);
+            Ok(())
+        });
 
     let mut rt = tokio::runtime::current_thread::Runtime::new().expect("new rt");
     rt.block_on(res).unwrap();
-
-    // println!("{}", );
-
-    // println!("Hello, world!");
 }
--- src/session.rs
+++ src/session.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "UPPERCASE")]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum SessionStatus {
     Initialized,
     Connected,
@@ -10,8 +11,36 @@ pub enum SessionStatus {
     Timeout,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ProofStatus {
+    /// Proof is valid
+    Valid,
+    /// Proof is invalid
+    Invalid,
+    /// Attribute-based signature had invalid timestamp
+    InvalidTimestamp,
+    /// Proof does not correspond to a specified request
+    UnmatchedRequest,
+    /// Proof does not contain all requested attributes
+    MissingAttributes,
+    /// Attributes were expired at proof creation time (now, or according to timestamp in case of abs)
+    Expired,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum AttributeProofStatus {
+    /// Attribute is disclosed and matches the value
+    Present,
+    /// Attribute is disclosed, but wasn't requested in request
+    Extra,
+    /// Attribute is disclosed but is null
+    Null,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "camelCase")]
 pub enum SessionType {
     Disclosing,
     Signing,
@@ -34,18 +63,31 @@ impl<'a> Into<&'a str> for &'a SessionToken {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct SessionPackage {
-    #[serde(rename = "sessionPtr")]
     pub session_ptr: Qr,
     pub token: SessionToken,
 }
 
-// https://godoc.org/github.com/privacybydesign/irmago/server#SessionResult
 #[derive(Serialize, Deserialize, Debug)]
+pub struct DisclosedAttribute {
+    rawvalue: Option<String>,
+    value: BTreeMap<String, String>,
+    id: String,
+    status: AttributeProofStatus,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct SessionResult {
-    token: SessionToken,
-    status: SessionStatus,
+    pub token: SessionToken,
+    pub status: SessionStatus,
     #[serde(rename = "type")]
-    stype: SessionType,
-    //
+    pub stype: SessionType,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub proof_status: Option<ProofStatus>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub disclosed: Option<Vec<Vec<DisclosedAttribute>>>,
+    // TODO signature irma.SignedMessage
+    // TODO error irma.RemoteError
 }
